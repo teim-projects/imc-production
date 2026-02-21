b2a1a89cb34bc6868dfe84be80bcd6d071f668aa
teim-projects <connectteim@gmail.com>
2026-02-21 11:40:02 +0530

    Updated

--- Frontend/imc/src/userDashboard/Forms/UserEvents.jsx
+++ Frontend/imc/src/userDashboard/Forms/UserEvents.jsx
@@ -14,7 +14,7 @@ import {
 
 /* ===================== CONFIG ===================== */
 
-const BASE = import.meta.env.VITE_BASE_API_URL || "http://127.0.0.1:8000";
+const BASE = import.meta?.env?.VITE_BASE_API_URL || "https://www.imcpune.in/api";
 
 const EVENTS_URL = `${BASE}/user/events/`;
 const BOOKINGS_URL = `${BASE}/user/event-bookings/`;
